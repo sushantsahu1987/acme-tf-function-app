d6936dc22f3a1e68d416f3948f1ab4f75fc91f28
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2024-04-16 12:54:52 +0530

    feat: implement tf for azure function resource creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 terraform apply -var-file=variables.tfvars
+
+az functionapp deployment list-publishing-profiles --name acme-test-function-app --resource-group acme-test-resource-group 
--- main.tf
+++ main.tf
@@ -22,3 +22,31 @@ resource "azurerm_resource_group" "acme-test-resource-group" {
   name     = "acme-test-resource-group"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "acme-test-storage-acc" {
+  name                     = "acmeteststorageacc"
+  resource_group_name      = azurerm_resource_group.acme-test-resource-group.name
+  location                 = azurerm_resource_group.acme-test-resource-group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "acme-test-sp" {
+  name                = "acme-test-as-plan"
+  resource_group_name = azurerm_resource_group.acme-test-resource-group.name
+  location            = azurerm_resource_group.acme-test-resource-group.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "acme-test-function-app" {
+  name                = "acme-test-function-app"
+  resource_group_name = azurerm_resource_group.acme-test-resource-group.name
+  location            = azurerm_resource_group.acme-test-resource-group.location
+
+  storage_account_name       = azurerm_storage_account.acme-test-storage-acc.name
+  storage_account_access_key = azurerm_storage_account.acme-test-storage-acc.primary_access_key
+  service_plan_id            = azurerm_service_plan.acme-test-sp.id
+
+  site_config {}
+}
